a31d63194f473e1b2afbd386df586b5cede90a68
krunalsukhwani <krunal.sukhwani@gmail.com>
2023-04-01 21:51:47 -0400

    Printing post data in console

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
@@ -6,25 +5,12 @@ import Modal from './Modal';
 
 function PostsList( { isPosting, onStopPosting } ) {
 
-    const [enteredBody, setEnteredBody] = useState('');
-
-    const [enteredAuthor, setEnteredAuthor] = useState('');
-
-    function bodyChangeHandler(event) {
-        setEnteredBody(event.target.value);
-    }
-
-    function authorChangeHandler(event) {
-        setEnteredAuthor(event.target.value);
-    }
-
     let modalContent;
 
     if (isPosting) {
         modalContent = <Modal onClose={onStopPosting}>
             <NewPost
-                onBodyChange={bodyChangeHandler}
-                onAuthorChange={authorChangeHandler}
+                onCancel={onStopPosting}
             />
         </Modal>;
     }
@@ -34,7 +20,6 @@ function PostsList( { isPosting, onStopPosting } ) {
             {modalContent}
 
             <ul className={classes.posts}>
-                <Post author={enteredAuthor} body={enteredBody} />
                 <Post author="James" body="I am business architect in IT company" />
                 <Post author="Sanjay" body="Software Developer" />
             </ul>

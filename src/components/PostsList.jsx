d47799dd81fed061a7d980ca4a7f8b43b3a960de
krunalsukhwani <krunal.sukhwani@gmail.com>
2023-04-01 21:20:15 -0400

    Close Modal when click on backdrop

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -6,26 +6,39 @@ import Modal from './Modal';
 
 function PostsList() {
 
-    const [ enteredBody, setEnteredBody ] = useState('');
+    const [modalIsVisible, setModalIsVisible] = useState(true);
 
-    const [ enteredAuthor, setEnteredAuthor ] = useState('');
+    const [enteredBody, setEnteredBody] = useState('');
 
-    function bodyChangeHandler(event){
+    const [enteredAuthor, setEnteredAuthor] = useState('');
+
+    function hideModalHandler() {
+        setModalIsVisible(false);
+    }
+
+    function bodyChangeHandler(event) {
         setEnteredBody(event.target.value);
     }
 
-    function authorChangeHandler(event){
+    function authorChangeHandler(event) {
         setEnteredAuthor(event.target.value);
     }
+
+    let modalContent;
+
+    if (modalIsVisible) {
+        modalContent = <Modal onClose={hideModalHandler}>
+            <NewPost
+                onBodyChange={bodyChangeHandler}
+                onAuthorChange={authorChangeHandler}
+            />
+        </Modal>;
+    }
+
     return (
         <>
-            <Modal>
-                <NewPost 
-                    onBodyChange={bodyChangeHandler} 
-                    onAuthorChange={authorChangeHandler}
-                />
-            </Modal>
-            
+            {modalContent}
+
             <ul className={classes.posts}>
                 <Post author={enteredAuthor} body={enteredBody} />
                 <Post author="James" body="I am business architect in IT company" />

f6305cc4f8069cc0b72a1ea39cf450c9e9100138
krunalsukhwani <krunal.sukhwani@gmail.com>
2023-04-01 20:03:05 -0400

    New post form

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,12 +1,17 @@
+import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 
 function PostsList() {
     return (
-        <ul className={classes.posts}>
-            <Post author="Krunal" body="I am lead in IT company" />
-            <Post author="James" body="I am business architect in IT company" />
-        </ul>
+        <>
+            <NewPost />
+            <ul className={classes.posts}>
+                <Post author="Krunal" body="I am lead in IT company" />
+                <Post author="James" body="I am business architect in IT company" />
+                <Post author="Sanjay" body="Software Developer" />
+            </ul>
+        </>
     );
 }
 

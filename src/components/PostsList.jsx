210647d32da57c261476a0002f1189f72d017870
krunalsukhwani <krunal.sukhwani@gmail.com>
2023-04-01 20:57:49 -0400

    Update Post Dynamically

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,29 @@
+import { useState } from 'react';
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 
 function PostsList() {
+
+    const [ enteredBody, setEnteredBody ] = useState('');
+
+    const [ enteredAuthor, setEnteredAuthor ] = useState('');
+
+    function bodyChangeHandler(event){
+        setEnteredBody(event.target.value);
+    }
+
+    function authorChangeHandler(event){
+        setEnteredAuthor(event.target.value);
+    }
     return (
         <>
-            <NewPost />
+            <NewPost 
+                onBodyChange={bodyChangeHandler} 
+                onAuthorChange={authorChangeHandler}
+            />
             <ul className={classes.posts}>
-                <Post author="Krunal" body="I am lead in IT company" />
+                <Post author={enteredAuthor} body={enteredBody} />
                 <Post author="James" body="I am business architect in IT company" />
                 <Post author="Sanjay" body="Software Developer" />
             </ul>
